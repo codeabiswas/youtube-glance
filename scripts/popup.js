7d87bdfcbac468e1aa77883c135e4d2905ddd989
codeabiswas <axb6972@rit.edu>
2024-06-07 11:07:51 +0530

    Can receive message in console log when I am hovering over a youtube link

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,1 +1,18 @@
-console.log("Hello world! This is a popup");
+// // console.log("Hello world! This is a popup");
+
+// document.onmouseenter = function (e) {
+//   console.log("hit something!");
+// };
+
+// Select all the links on the page
+var links = document.querySelectorAll("a");
+var youtubeLinks = Array.from(links).filter((link) =>
+  link.href.includes("youtube")
+);
+
+// Add an event listener to each link
+for (var i = 0; i < youtubeLinks.length; i++) {
+  youtubeLinks[i].addEventListener("mouseenter", function () {
+    console.log("Mouse entered a link");
+  });
+}
